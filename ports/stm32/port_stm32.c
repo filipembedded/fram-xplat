bd5cde9df37863475584fc6a37eaa8f2a570c1a1
Filip Stojanovic <filipembedded@gmail.com>
2025-10-12 12:29:19 +0200

    ports(stm32_hal): Added initial port for STM32 HAL

--- ports/stm32/port_stm32.c
+++ ports/stm32/port_stm32.c
@@ -3,3 +3,42 @@
  * @brief STM32 HAL port for FRAM-XPLAT library 
  * @author Filip Stojanovic
  */
+
+#include "stm32f4xx_hal.h"
+
+typedef struct {
+    GPIO_TypeDef cs_port;
+    uint16_t cs_pin;
+} GPIO_CS_Config;
+
+int spi_write(SPI_HandleTypeDef *hspi, uint8_t *data, uint16_t size) {
+    HAL_StatusTypeDef status = HAL_SPI_Transmit(hspi, data, size, SPI_TIMEOUT_MS);
+    if (status != HAL_OK) {
+        return -1;
+    }
+    return 0;
+}
+
+int spi_read(SPI_HandleTypeDef *hspi, uint8_t *data, uint16_t size) {
+    HAL_StatusTypeDef status = HAL_SPI_Receive(hspi, data, size, SPI_TIMEOUT_MS);
+    if (status != HAL_OK) {
+        return -1;
+    }
+    return 0;
+}
+
+int spi_chip_select(GPIO_CS_Config *gpio) {
+    HAL_StatusTypeDef status = HAL_GPIO_WritePin(gpio->cs_port, gpio->cs_pin, GPIO_PIN_RESET);
+    if (status != HAL_OK) {
+        return -1;
+    }
+    return 0;
+}
+
+int spi_chip_deselect(GPIO_CS_Config *gpio) {
+    HAL_StatusTypeDef status = HAL_GPIO_WritePin(gpio->cs_port, gpio->cs_pin, GPIO_PIN_SET);
+    if (status != HAL_OK) {
+        return -1;
+    }
+    return 0;
+}
--- ports/stm32/port_stm32.h
+++ ports/stm32/port_stm32.h
@@ -3,3 +3,13 @@
  * @brief STM32 HAL port for FRAM-XPLAT library 
  * @author Filip Stojanovic
  */
+
+#define SPI_TIMEOUT_MS 50
+
+int spi_write(SPI_HandleTypeDef *hspi, uint8_t *data, uint16_t size);
+
+int spi_read(SPI_HandleTypeDef *hspi, uint8_t *data, uint16_t size);
+
+int spi_chip_select();
+
+int spi_chip_deselect();
